e56de7d9c48ad3e4daa136adbfca960521dbd08d
Altaf Khan <89717257+Altaf1k1P@users.noreply.github.com>
2025-06-26 17:08:46 +0530

    Update Footer.js

--- components/Layouts/Footer.js
+++ components/Layouts/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import logo from "../../public/images/white-logo.png";
+import logo from "../../public/images/syntheon-logo.png";
 import shape1 from "../../public/images/shape/footer-shape-one.png";
 import shape2 from "../../public/images/shape/footer-shape-two.png";
 
